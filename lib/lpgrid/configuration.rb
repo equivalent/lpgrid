787944c81ca0de4d2cd34d77fa26681c4df4e6c0
Tomas Valent <equivalent@eq8.eu>
2019-03-16 12:19:39 +0100

    default home folder

--- lib/lpgrid/configuration.rb
+++ lib/lpgrid/configuration.rb
@@ -5,12 +5,24 @@ module Lpgrid
       Lpgrid.root.join('config.yml')
     end
 
-    def options
-      @options ||= YAML.load_file(file_path)
+    def csv
+      return options['csv_path'] if options['csv_path']
+
+      home_dot_grid = "#{ENV['HOME']}/.grid.csv"
+      return home_dot_grid if File.exist?(home_dot_grid)
+
+      home_grid = "#{ENV['HOME']}/grid.csv"
+      return home_grid if File.exist?(home_grid)
+
+      raise "You need to define grid path in project 'config.yml' or just move your grid to '~/.grid.csv'"
     end
 
-    def csv
-      options.fetch('csv_path')
+    private
+
+    def options
+      @options ||= YAML.load_file(file_path)
+    rescue Errno::ENOENT
+      {}
     end
   end
 end
